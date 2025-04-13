e585e68c2e746917b4edfe3982c00b07068f3aed
Beatris <beatrisraj7@gmail.com>
2025-04-13 18:53:49 +0300

    Top navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { Element } from "react-scroll"
 import Sidebar from "./components/sidebar/SideNavBar"
+import TopNavigation from "./components/Navigation"
 
 function App() {
 
   return (
     <div className="flex">
-  {/* Sidebar */}
-  <div className="w-64 fixed h-screen bg-[#12121B]/90 text-white z-10">
-    <Sidebar />
-  </div>
+      <div className="w-64 fixed h-screen bg-[#12121B]/90 text-white z-10">
+        <Sidebar />
+      </div>
 
-  {/* Page Content */}
-  <div className="ml-64 flex-1">
-    <Element name="home" className="min-h-screen bg-white p-8">
-      Home Section
-    </Element>
-    <Element name="tech" className="min-h-screen bg-gray-100 p-8">
-      Tech Section
-    </Element>
-    {/* Add more sections as needed */}
-  </div>
-</div>
+      {/* Page Content */}
+      <div className="ml-64 flex-1">
+        <TopNavigation />
+        <Element name="home" className="min-h-screen bg-white p-8">
+          Home Section
+        </Element>
+        <Element name="tech" className="min-h-screen bg-gray-100 p-8">
+          Tech Section
+        </Element>
+      </div>
+    </div>
   )
 }
 
