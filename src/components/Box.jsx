24a21d273ef3bd407712ec34a7c0fd21474f02c1
Beatris <beatrisraj7@gmail.com>
2025-04-13 22:45:27 +0300

    Glowing border

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,18 +1,19 @@
-export default function Box({children, headther, className = ""}) {
-   return (
-    <div className={`bg-[#12121b]/80 ${className}`} >
-        {headther && (
-        <div className={`border-b-4 border-indigo-500 w-full ${
-            headther.textPosition === 'center' ?
-            'text-center':
-            'text-start'
-        }`} >
-            <h1 className="jersay">{headther.title}</h1>
+
+export default function Box({ children, headther, className = "" }) {
+    return (
+        <div
+            className={`glow-border bg-[#12121b]/80 z-0 relative p-4 ${className}`} >
+
+            {headther && (
+                <div className={`border-b-4 border-indigo-500 w-full ${headther.textPosition === 'center' ?
+                        'text-center' :
+                        'text-start'
+                    }`} >
+                    <h1 className="jersay">{headther.title}</h1>
+                </div>
+            )}
+
+            {children}
         </div>
-        )}
-
-        {children}
-
-    </div>
-);
+    );
 }
