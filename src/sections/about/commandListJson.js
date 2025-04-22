301efbdceb6e5a433b09cb720ff36f7464de0119
Beatris <beatrisraj7@gmail.com>
2025-04-22 17:36:43 +0300

    Lists of commands

---
+++ src/sections/about/commandListJson.js
@@ -0,0 +1,15 @@
+export const personalCommandList = [
+    { name: 'curl currently-learning', description:'Load current studies'},
+    { name: 'npm hobbies', description:'Load  hobbies'},
+    { name: 'npm run fun-fact.js', description:'Returns a random fact about me'},
+    { name: 'git status', description:'Git Status'},
+    { name: 'cd system', description:'Show system list of commands'},
+]
+
+export const SystemCommnadList = [
+    { name: 'help', description:'Lists all commands'},
+    { name: 'clear', description:'Clears terminal'},
+    { name: 'ls', description:'Lists content of site'},
+    { name:'scan', description:'Scans for threats'},
+    { name:'cd personal', description:'Show personal list of commands'},
+]
