2e55814e152b50fdef233e2366853884485c24f8
Beatris <beatrisraj7@gmail.com>
2025-04-22 19:25:10 +0300

    History and input terminal

--- src/sections/about/terminal/Terminal.jsx
+++ src/sections/about/terminal/Terminal.jsx
@@ -2,31 +2,42 @@ import { useState } from "react";
 
 export default function Terminal({ onChangeCommand }) {
     const [input, setInputValue] = useState('')
+    const [history, setHistory] = useState([])
 
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
-            console.log("User entered:", input);
+            onChangeCommand(input)
+            setHistory(prev => [...prev, input])
 
             setInputValue("");
         }
     };
 
     return (
-        <div className="bg-[#11121B]/80 w-1/2">
+        <div className="bg-[#11121B]/80 w-1/2 h-70 flex flex-col">
             <h1 className="p-4 text-center jersay text-xl">Powershell</h1>
-            <div className="h-50">
-                <div className="flex gap-2 p-2">
-                    <p>{"C:\\Betty\\about>"}</p>
-                    <input
-                        type="text"
-                        placeholder="Type something..."
-                        className="outline-none border-none focus:ring-0 focus:border-transparent"
-                        value={input}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                    ></input>
-                </div>
 
+            <div className="flex-1 overflow-y-auto p-2">
+                <ul>
+                    {history.map((el, idx) => (
+                        <li key={idx} className="flex gap-2">
+                            <p className="text-gray-300">{"C:\\Betty\\about>"}</p>
+                            <span>{el}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flex gap-2 p-2 border-t border-white/10">
+                <p>{"C:\\Betty\\about>"}</p>
+                <input
+                    type="text"
+                    placeholder="Type something..."
+                    className="flex-1 bg-transparent text-pink-500 outline-none border-none focus:ring-0 focus:border-transparent"
+                    value={input}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
             </div>
         </div>
     );
