1303602818de048217608ea76feb2b7c79ef0534
Beatris <beatrisraj7@gmail.com>
2025-04-22 17:09:13 +0300

    Content box

--- src/sections/about/AboutSection.jsx
+++ src/sections/about/AboutSection.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Element } from "react-scroll";
+import Content from "./content/Content";
 
 export default function About() {
+    const [currentCommand, setCurrentCommand] = useState('')
    return (
-       <Element name="about" className="min-h-screen">
+       <Element name="about" className="min-h-screen p-8">
+        <div>
+            <Content command={currentCommand} />
+        </div>
+        <div>
+
+        </div>
 
        </Element>
    );
---
+++ src/sections/about/content/Content.jsx
@@ -0,0 +1,25 @@
+export default function Content({command}) {
+
+   return (
+      <div className="w-3/5 h-[30rem] bg-[#11121B]/80">
+         <div className="w-full bg-[#11121B]/80 p-2">
+            <p className="jersay text-xl px-2">Content</p>
+         </div>
+         <div className="relative w-full h-11/12">
+            {command.trim() === '' && <WaitingState />}
+         </div>
+      </div>
+   );
+}
+
+function WaitingState(){
+   return(
+      <div className="flex items-end gap-2 absolute bottom-2 right-10 text-[#868383]">
+      <p>Waiting for command...</p>
+      <pre className="scale-x-[-1]">
+{` /)/)
+(.  .)`}
+      </pre>
+      </div>
+   )
+}
