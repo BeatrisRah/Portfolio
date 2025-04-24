517270820a7bd2fa7ee6a049d9fb41d4d38f05ba
Beatris <beatrisraj7@gmail.com>
2025-04-24 18:37:22 +0300

    hobbies

--- src/sections/about/content/ContentElements.jsx
+++ src/sections/about/content/ContentElements.jsx
@@ -1,46 +1,19 @@
 
 import InvalidCommandTerminal from "./InvalidCommand";
 import CurrentlyLearning from "./CurrenlyLearning";
+import Hobbies from "./Hobbies";
 import FunFact from "./FunFact";
+import HelpCommandContent from "./HelpCommand";
 
 
 
+export default function ContentElements({ command }) {
 
     switch (command) {
         case "curl currently-learning":
             return <CurrentlyLearning />
         case "npm i hobbies":
-            return (
-                <div className="text-white p-6  font-mono border-t border-green-400 space-y-1">
-                    <p className="text-green-400">➤ Installing hobbies@latest...</p>
-                    <p className="text-green-400">➤ Fetching metadata...</p>
-                    <p className="text-green-400">➤ Resolving packages...</p>
-
-                    <ul className="pl-4 space-y-1">
-                        <li>
-                            <span className="text-yellow-300">+ music-jams@1.2.0</span> 🎧 –
-                            playlists for every coding mood
-                        </li>
-                        <li>
-                            <span className="text-pink-400">+ pixel-art@0.8.7</span> 🎨 –
-                            drawing tiny worlds, one pixel at a time
-                        </li>
-                        <li>
-                            <span className="text-blue-300">+ night-coding@3.5.1</span> 🌙 –
-                            boosted by lo-fi & caffeine
-                        </li>
-                        <li>
-                            <span className="text-rose-400">+ cozy-crocheting@2.3.0</span> 🧶
-                            – creating more than code
-                        </li>
-                    </ul>
-
-                    <p className="text-green-400">➤ Installed 4 packages in 2.7s</p>
-                    <p className="text-green-400">
-                        ➤ You’re now ready to relax and vibe ✨
-                    </p>
-                </div>
-            );
+            return <Hobbies />
         case "npm run fun-fact.js":
             return <FunFact />
         case 'cd system':
