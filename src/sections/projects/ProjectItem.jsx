b5a6b53307226e71a442b4b4123bb666cfd9e8e4
Beatris <beatrisraj7@gmail.com>
2025-04-25 19:18:18 +0300

    RESPONSIVE UI/ Projects
    1024px >= screens

--- src/sections/projects/ProjectItem.jsx
+++ src/sections/projects/ProjectItem.jsx
@@ -14,24 +14,24 @@ export default function ProjectItem({ project }) {
         setActiveTab(tab)
     }
 
-    const width = project.grow > 0 ? 'w-[50%]' : 'w-[40%]'
+    const width = project.grow > 0 ? 'w-[55%] xl:w-[50%]' : 'w-[40%]'
 
     return (
         <Box
             headther={{ title: project.name, marginB:4 }}
-            className={`${width} h-[26rem]`}
+            className={`${width} min-h-[26rem]`}
         >
-            <div className="flex flex-wrap w-full overflow-hidden h-full p-2">
+            <div className="flex flex-wrap w-full  p-2">
                 <div className="w-1/2 h-1/2">
                     <TabNav onChange={onChange} activeTab={activeTab} />
                     {activeTab === 'Info' && <InfoTab info={project.info} />}
                     {activeTab === 'Features' && <FeaturesTab featuresArr={project.features} />}
                     {activeTab === 'Links' && <LinksTab links={project.links} />}
                 </div>
-                <div className="w-1/2 h-1/2">
-                    <img src={project.image} className="w-80 mx-auto h-full object-scale-down" />
+                <div className="w-1/2 h-1/2 mt-3">
+                    <img src={project.image} className="w-80 mx-auto h-50 object-scale-down" />
                 </div>
-                <div className="w-full flex flex-wrap gap-2 h-20">
+                <div className="w-full flex flex-wrap gap-2 mt-5">
                 {project.techStack.map(t => <TechItem tech={t} />)}
                 </div>
             </div>
--- src/sections/tech/TechItem.jsx
+++ src/sections/tech/TechItem.jsx
@@ -1,6 +1,6 @@
 export default function TechItem({tech}) {
    return (
-    <div key={tech.name} className="flex gap-2 w-40 p-2 h-10 tech-border">
+    <div key={tech.name} className="flex gap-2 p-2 h-10 tech-border min-w-[8rem] max-w-[10rem]">
         <img src={tech.img} className="w-6 h-6" />
         <p>{tech.name}</p>
     </div>
